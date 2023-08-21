f1faab32fcef82bd3c039e7d678b161cc61be8fc
Bryan Andrew Castro Valencia <123597153+4ndrewxyz@users.noreply.github.com>
2023-08-21 10:40:36 -0600

    primer commit

---
+++ php/templates/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP CRUD</title>
+    <link rel="stylesheet" type="text/css" href="/public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/materialize.css">
+</head>
+<body>
+<div class="title">
+    <h1>Pagina web Bryan Andrew Castro Valencia</h1>
+</div>
+   <script type="text/javascript" src="/public/js/materialize.js"></script>
+</body>
+</html>
